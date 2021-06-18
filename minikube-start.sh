21ed32a00ceb27a2e0549c92e646c1765d4a4e84
cscotti <cscotti@monoprix.fr>
2021-06-19 01:05:59 +0200

    Create minikube-start.sh

---
+++ minikube-start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#rm -Rf ~/.minikube
+minikube config set cpus 2
+minikube config set memory 4096
+
+minikube start
+minikube ip
+
+# https://github.com/kubernetes/minikube/issues/8283
+ip_range=$(minikube ip | sed -r 's/(.*)./\1/')
+cat ~/.minikube/profiles/minikube/config.json  \
+    | jq '.KubernetesConfig.LoadBalancerStartIP="'${ip_range}105'"' \
+    | jq '.KubernetesConfig.LoadBalancerEndIP="'${ip_range}120'"'   \
+ > ~/.minikube/profiles/minikube/config.json.tmp \
+    && mv ~/.minikube/profiles/minikube/config.json.tmp ~/.minikube/profiles/minikube/config.json 
+
+minikube addons enable metallb
+minikube addons configure metallb
+
+minikube addons enable metrics-server
+minikube addons enable dashboard
+minikube addons enable helm-tiller
+minikube addons enable ingress
+minikube addons enable ingress-dns
+k get svc kubernetes-dashboard -n kubernetes-dashboard -o json \
+| jq '.spec.type="LoadBalancer"' \
+| jq '.spec.ports[0].port=8443' | k apply -f -
+
+# open the dashboard directly
+$ DASHBOARD_URL=http://$(kubectl get svc kubernetes-dashboard  -n kubernetes-dashboard --no-headers | awk '{print $4}'):8080
+$ echo "$DASHBOARD_URL"
+$ xdg-open $DASHBOARD_URL
