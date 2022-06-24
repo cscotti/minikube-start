d6b05353d3cea36ff29d5e5ac2ec2b124266d080
cscotti <scotti.carlo@gmail.com>
2022-06-24 23:16:39 +0200

    Update minikube-start.sh

--- minikube-start.sh
+++ minikube-start.sh
@@ -6,6 +6,7 @@
 
 minikube config set cpus 2
 minikube config set memory 4096
+# minikube config set driver docker
 minikube config set vm-driver hyperkit
 minikube config set kubernetes-version v1.21.11
 #minikube config set WantVirtualBoxDriverWarning false
